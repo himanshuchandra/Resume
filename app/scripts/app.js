c7a25d765adbc410d10bc2134a2b02943b09f3ab
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-04-29 12:05:59 +0530

    yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name resApp
+ * @description
+ * # resApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('resApp', [
+    'ngAnimate'
+  ]);
